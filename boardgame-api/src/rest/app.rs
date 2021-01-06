5d2bcf15f3097d2f17592a7ebdaafa198ec6375e
a-higa <ahigachika@gmail.com>
2021-01-06 23:35:37 +0900

    routeをつくって関数に処理を分割した

--- boardgame-api/src/rest/app.rs
+++ boardgame-api/src/rest/app.rs
@@ -1,7 +1,13 @@
 use tide::Server;
+use super::systems;
 
 pub fn get_app() -> Server<()> {
   let mut app = tide::new();
-  app.at("/").get(|_| async { Ok("Hello, world!") });
+  app = add_route(app);
+  app
+}
+
+pub fn add_route(mut app: Server<()>) -> Server<()> {
+  app.at("/v1/systems/ping").get(|_| async move { systems::ping() });
   app
 }
