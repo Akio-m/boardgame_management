fe29f32958e0be9e472e2d10b7e6db58b7b9949d
a-higa <ahigachika@gmail.com>
2021-01-08 23:22:53 +0900

    boardgameJsonを返せるようになった

--- boardgame-api/src/rest/boardgames.rs
+++ boardgame-api/src/rest/boardgames.rs
@@ -1,10 +1,20 @@
 use serde_json::json;
+use serde::{Serialize, Deserialize};
 use tide::{Response, Result, StatusCode};
 
 use crate::usecase::get_boardgames;
+use crate::domain::boardgames::Boardgames;
 
 pub async fn getBoardgames() -> Result<> {
-  // let boardgames = get_boardgames.execute().await;
-  let body = json!("a");
+  let boardgames = get_boardgames::execute().await.unwrap();
+  let body = json!(BoardgamesJson::from(boardgames));
   Ok(Response::builder(StatusCode::Ok).body(body).build())
+}
+#[derive(Serialize, Deserialize, Debug)]
+struct BoardgamesJson {}
+
+impl BoardgamesJson {
+  fn from(boardgames: Boardgames) -> Self {
+    BoardgamesJson {}
+  }
 }
